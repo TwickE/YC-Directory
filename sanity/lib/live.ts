f65821b1e17a8b73940bcbd9880ca98d21aee100
TwickE <118013263+TwickE@users.noreply.github.com>
2024-12-20 15:12:09 +0000

    Refactor Sanity client usage and integrate live content fetching

--- sanity/lib/live.ts
+++ sanity/lib/live.ts
@@ -1,4 +1,4 @@
-// Querying with "sanityFetch" will keep content automatically updated
+/* // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
 import { defineLive } from "next-sanity";
@@ -10,4 +10,11 @@ export const { sanityFetch, SanityLive } = defineLive({
     // https://www.sanity.io/docs/api-versioning
     apiVersion: 'vX' 
   }) 
-});
+}); */
+
+import "server-only"
+
+import { defineLive } from "next-sanity"
+import { client } from "@/sanity/lib/client"
+
+export const { sanityFetch, SanityLive } = defineLive({ client })
